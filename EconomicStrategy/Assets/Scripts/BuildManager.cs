49fdd9b5f909920104f29f74c375b2a484cce376
pipaisopa <jenyaleo619828@gmail.com>
2021-09-21 21:39:36 +0300

    Первоначальный загруз

---
+++ EconomicStrategy/Assets/Scripts/BuildManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BuildManager : MonoBehaviour
+{
+    public bool BuiltBy;
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    void OnMouseEnter()
+    {
+        if (BuiltBy == true)
+        {
+            transform.GetChild(0).GetComponent<Image>().color = Color.red;
+        }
+        else
+        {
+            transform.GetChild(0).GetComponent<Image>().color = Color.green;
+        }
+        
+    }
+
+    void OnMouseExit()
+    {
+        transform.GetChild(0).GetComponent<Image>().color = Color.white;
+    }
+}
